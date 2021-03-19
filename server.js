496a1b36e46aa1c2b9a7dd8924965b0b914c6fa4
彭建添(MG01982) <pengjiantian@huijintech.com.cn>
2021-03-19 09:48:39 +0800

    构建开发模式

--- server.js
+++ server.js
@@ -2,40 +2,54 @@
  * 通用应用 Web 服务启动脚本
  */
 const express = require('express')
-const Vue = require('vue')
+const path = require('path')
 const fs = require('fs')
 
-const serverBundle = require('./dist/vue-ssr-server-bundle.json')
-const template = fs.readFileSync('./index.template.html', 'utf-8')
-const clientManifest = require('./dist/vue-ssr-client-manifest.json')
+const isProd = process.env.NODE_ENV === 'production'
+// 生成一个渲染器
+let renderer
+if (isProd) {
+  // 生产模式
+  const serverBundle = require('./dist/vue-ssr-server-bundle.json')
+  const template = fs.readFileSync('./index.template.html', 'utf-8')
+  const clientManifest = require('./dist/vue-ssr-client-manifest.json')
+  renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
+    template,
+    clientManifest,
+  })
+} else {
+  // 开发模式:打包构建(客户端+服务端) -> 创建渲染器
+}
+
 // 创建一个 express 实例
 const server = express()
-// 生成一个渲染器
-const renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
-  template,
-  clientManifest,
-})
+server.use(express.static(path.resolve(__dirname, '/dist')))
 
-server.use('/dist', express.static('./dist'))
+const render = async (req, res) => {
+  try {
+    const html = await renderer.renderToString(
+      {
+        title: '测试',
+        meta: `<meta name="viewport" content="测试">`,
+        url: req.url, // 路由路径
+      } // context
+    )
+    res.setHeader('Content-Type', 'text/html; charset=utf8')
+    res.end(html)
+  } catch (error) {
+    return res.status(500).end(err.msg)
+  }
+}
 
 // 设置一个路由
-server.get('/', (req, res) => {
-  renderer.renderToString(
-    {
-      title: 'vue-ssr',
-      meta: `
-	   <meta name="ssr" content="vue-ssr">
-	  `,
-    },
-    (err, html) => {
-      if (err) {
-        return res.status(500).end('Internal Server Error.')
+server.get(
+  '*',
+  isProd
+    ? render // 生产模式 => 使用构建好的包直接渲染
+    : (req, res) => {
+        // 开发模式 => 等编译构建好再渲染
       }
-      res.setHeader('Content-Type', 'text/html; charset=utf8')
-      res.end(html)
-    }
-  )
-})
+)
 // 监听端口，启动 Web 服务
 server.listen(3000, () => {
   console.log('http://localhot:3000.')
